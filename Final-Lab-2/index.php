d838e0398cfc7da69cc86dd43510fb86f831eb2b
Rifat Miah <152371153+Rifat-Miah@users.noreply.github.com>
2026-04-15 14:01:52 +0600

    public home page added

---
+++ Final-Lab-2/index.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>XCompany</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="main-box">
+
+    <h1>Welcome to X Company</h1>
+
+    <div class="menu">
+        <a href="register.php">Registration</a>
+        <a href="login.php">Login</a>
+        <a href="forgot.php">Forgot Password</a>
+    </div>
+
+</div>
+
+</body>
+</html>
